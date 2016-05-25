c6de602059b5845089eefd092572c59e541b0164
jpmarindiaz <jpmarindiaz@gmail.com>
2016-05-25 18:35:28 -0500

    jpmarindiaz solution

---
+++ 2016-05-25-golfito-asesinos-seriales/solutions/jpmarindiaz.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(dmaps)
+library(shiny)
+library(DT)
+app <- shinyApp(
+  ui = bootstrapPage(
+    dmapsOutput("viz",height = "400"),dataTableOutput("table"),verbatimTextOutput("debug")),
+  server = function(input, output) {
+    data <- read.csv("serial-killers-clean.csv") %>% group_by(Country,Name) %>%
+      summarise(victims = sum(victims)) %>% group_by(Country) %>% filter(!is.na(victims)) %>%
+      summarise(total=sum(victims),nkillers=n()) %>% mutate(victimsPerKiller=total/nkillers) %>% arrange(desc(victimsPerKiller)) %>% as.data.frame()
+   output$debug <- renderPrint({paste(input$table_rows_selected,data[input$table_rows_selected,"Country"])})
+     output$viz <- renderDmaps({
+      region <- reactive(data[input$table_rows_selected,"Country"])
+      dmaps("world_countries",data = data,valueCol = "victimsPerKiller",
+            regionCols = c("Country"),regions = NULL)})
+    output$table <- renderDataTable({datatable(data)})})
+runApp(app)
